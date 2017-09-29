2b81eaa1b87cfb3131748c664df1ded230a26bb2
iwatakeshi <iwatakeshi@gmail.com>
2017-09-28 18:22:05 -0600

    Update

--- matrix.c
+++ matrix.c
@@ -8,43 +8,43 @@
 
 /* Matrix helper functions */
 
-void printm(int** matrix, unsigned int n, unsigned int m) {
+void printm(int** a, unsigned int n, unsigned int m) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      printf(" %2d", matrix[i][j]);
+      printf(" %2d", a[i][j]);
     }
     printf("\n");
   }
 }
 
-void zerom(int** matrix, unsigned int n, unsigned int m) {
+void zerom(int** a, unsigned int n, unsigned int m) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      matrix[i][j] = 0;
+      a[i][j] = 0;
     }
   }
 }
 
-void copym(int** matrix, int** matrix2, unsigned int n, unsigned int m) {
+void copym(int** a, int** b, unsigned int n, unsigned int m) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      matrix2[i][j] = matrix[i][j];
+      b[i][j] = a[i][j];
     }
   }
 }
 
 void * mallocm(unsigned int n, unsigned int m) {
-  int ** matrix = (int**)malloc(m * sizeof(int*));
+  int ** a = (int**)malloc(m * sizeof(int*));
   int i = 0;
   for (i = 0; i < m; i++) {
-    matrix[i] = malloc(n * sizeof(int));
+    a[i] = malloc(n * sizeof(int));
   }
-  return (void *)matrix;
+  return (void *)a;
 }
 
-void freem(int** matrix, unsigned int n) {
+void freem(int** a, unsigned int n) {
   for (int i = 0; i < n; i++) {
-    free(matrix[i]);
+    free(a[i]);
   }
-  free(matrix);
+  free(a);
 }
--- matrix.h
+++ matrix.h
@@ -5,10 +5,10 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 /* Matrix helper functions */
-void printm(int** matrix, unsigned int n, unsigned int m);
-void zerom(int** matrix, unsigned int n, unsigned int m);
-void copym(int** matrix, int** matrix2, unsigned int n, unsigned int m);
+void printm(int** a, unsigned int n, unsigned int m);
+void zerom(int** a, unsigned int n, unsigned int m);
+void copym(int** a, int** b, unsigned int n, unsigned int m);
 void * mallocm(unsigned int n, unsigned int m);
-void freem(int** matrix, unsigned int n);
+void freem(int** a, unsigned int n);
 
 #endif
