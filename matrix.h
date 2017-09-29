aa91573e8982554ebc40ba2ad88d75bb4e842ac2
Takeshi <iwatakeshi@users.noreply.github.com>
2017-09-28 20:07:17 -0600

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -8,7 +8,7 @@
 /* Matrix helper functions */
 void printm(int** a, unsigned int n, unsigned int m);
 void zerom(int** a, unsigned int n, unsigned int m);
-void randm(int **a, int n, int m, int h);
+void randm(int** a, int n, int m, int h);
 void copym(int** a, int** b, unsigned int n, unsigned int m);
 void * mallocm(unsigned int n, unsigned int m);
 void freem(int** a, unsigned int n);
